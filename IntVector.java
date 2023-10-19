d8dc133ccc096742ae8671eb46d25d48b7b9a42c
BigDawg13 <connordeady25@amdg.rockhursths.edu>
2023-10-19 12:26:55 -0500

    Initial Code

---
+++ IntVector.java
@@ -0,0 +1,23 @@
+public class IntVector {
+
+    private final static int INITIAL_CAPACITY = 10;
+    private int[] arr = new int[INITIAL_CAPACITY];
+    private int size = 0;
+
+    // default constructor
+    public IntVector() { }
+
+    // overloaded constructor
+    public IntVector(int capacity) {
+        if(capacity < 0) {
+            throw new IllegalArgumentException();
+        }
+        arr = new int[capacity];
+    }
+
+    // the size function, returns the size
+    public int size() { return size; }
+
+    
+
+}
