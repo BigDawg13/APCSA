6fc621a1d33acf779b262e9f5469062f9cafea50
BigDawg13 <connordeady25@amdg.rockhursths.edu>
2023-11-02 11:17:24 -0500

    Final Code IntVector

--- IntVector.java
+++ IntVector.java
@@ -18,6 +18,92 @@ public class IntVector {
     // the size function, returns the size
     public int size() { return size; }
 
-    
+    private void testBounds(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void tryResize() {
+        if(size >= arr.length - 1) { // We've reached the end of the array
+        // make a new temp array that's bigger than the origional
+            int[] temp = new int[1 + arr.length * 2];
+        // copy data into temp array
+            for(int i = 0; i < size; i++) {
+                temp[i] = arr[i];
+            }
+        // set old array = new temp array
+            arr = temp;
+        }
+    }
+
+    // CRUD = Create, read, update, delete
+    public int get(int index) {
+        testBounds(index);
+        return arr[index];
+    }
+
+    // insert the valie 'n' at the end of the vector
+    public void add(int n) {
+        tryResize(); // guarantee capacity first
+        arr[size++] = n;
+    }
+
+    public void clear() {
+        arr = new int[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void set(int index, int value) {
+        testBounds(index);
+        arr[index] = value;
+    }
+
+    /*
+     * sets the element at 'index' equal to 'value'
+     * returns true if the value was successfully inserted, false otherwise
+     * @param index The position in whihch the value will in inserted
+     * @param value The numerical value to be added
+     */
+
+    public boolean add(int index, int value) {
+        testBounds(index);
+        tryResize();
+        for(int i = size; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = value;
+        size++;
+        return true;
+    }
+
+
+    /*
+     * Removes the first instance of 'value' found in the list
+     * returns 'true' if the value was removed from the list, 'false' otherwise
+     * the list remains unchanged if the values does not exist in the list
+     * @param value The value to be removed if it exists
+     */
+    public boolean remove(int value) {
+        boolean found = false;
+        for(int i = 0; i < size - 1; i++) {
+            if(arr[i] == value || found) {
+                found = true;
+                arr[i] = arr[i + 1];
+            }
+        }
+        if(found) size--;
+        return found;
+    }
+
+    @Override
+    public String toString() {
+        if(size == 0) return "[]";
+        String t = "[" + arr[0];
+        for(int i = 1; i < size; i++) {
+            t += ", " + arr[i];
+        }
+        return t + "]";
+    }
 
 }
--- Start.java
+++ Start.java
@@ -1,18 +1,29 @@
 public class Start {
     public static void main(String[] args) {
         try {
-            IntVector test = new IntVector(1000);
+            IntVector test = new IntVector(); // Throw Illegal
 
-            System.out.println("Size: " + test.size());
+            System.out.println("Size: " + test.size()); // Size: 0
 
-            for(int i = 0; i < test.size(); i++) {
-    
+            for(int i = 0; i < 20; i++) {
+                test.add(1);
             }
+
+            // set first vaue to 50, shifts everything right
+            test.add(7, 50); // [50, 0, 1, 2, ... 19]
+            test.add(9, 50);
+            System.out.println(test); // [0...19]
+            test.remove(50);
+            System.out.println(test);
+
+            // remove thre first zero value found at index 1
+            //test.remove(0); // [50, 1, 2, ... 19]
+
         } catch(IllegalArgumentException ex) {
             System.out.println("That's illegal");
         } catch(ArithmeticException ex) {
             System.out.println("Bad math");
-        } catch(Exception ex) {
+        } catch(Exception ex) { // catch everything, last effort
             System.out.println("Generic exceptionm");
         }
     }
