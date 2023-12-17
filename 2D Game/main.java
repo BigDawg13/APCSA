99102047c88620d1c609fea0f888c58f106b3fc3
BigDawg13 <148258713+BigDawg13@users.noreply.github.com>
2023-12-16 23:07:20 -0600

    Update Main.java - Final Commit From Home
    I did this four hours ago, I have no idea why it didn't save and tbh I'm kinda pissed at github rn.

--- 2D Game/main.java
+++ 2D Game/main.java
@@ -13,9 +13,9 @@ public class Main {
 
 
 
-       Player MustardP = new Player("M");
-       Player ScarletP = new Player("S");
-       Player PlumP = new Player("P");
+       Player MustardP = new Player("M", "Outside");
+       Player ScarletP = new Player("S", "Outside");
+       Player PlumP = new Player("P", "Outside");
 
        MustardP.setRow(1);
        MustardP.setCol(7);
@@ -54,6 +54,8 @@ public class Main {
        Card rope = new Card("Weapon", "Rope");
 
 
+       ArrayList<Card> ENDGAME = new ArrayList<>();
+
        // Create a large ArrayList of Card objects
         ArrayList<Card> deck = new ArrayList<>();
         deck.add(mustard);
@@ -61,6 +63,10 @@ public class Main {
         deck.add(green);
         deck.add(white);
         deck.add(scarlet);
+        int rand1 = (int)(Math.random() * 5);
+        int randC = rand1;
+        ENDGAME.add(deck.get(randC));
+
         deck.add(study);
         deck.add(library);
         deck.add(billiard_room);
@@ -71,53 +77,249 @@ public class Main {
         deck.add(stairs);
         deck.add(lounge);
         deck.add(dining_room);
+        int rand2 = (int)(Math.random() * 11) + 4;
+        int randR = rand2;
+        ENDGAME.add(deck.get(randR));
+
         deck.add(candlestick);
         deck.add(dagger);
         deck.add(revolver);
         deck.add(wrench);
         deck.add(rope);
+        int rand3 = (int)(Math.random() * 5) + 14;
+        int randW = rand3;
+        ENDGAME.add(deck.get(randW));
 
         // Shuffle the large ArrayList
         Collections.shuffle(deck);
 
+
+        CardSet d = new CardSet(deck);
         // Declare and initialize ArrayLists with the specified fashion
 
         // Distribute cards into three ArrayLists
         distributeCards(deck, MustardP.getHand(), ScarletP.getHand(), PlumP.getHand());
+
+        
 
         // Print the results
         System.out.println("Mustard Hand: " + MustardP.getHandString());
         System.out.println("Scarlet Hand: " + ScarletP.getHandString());
         System.out.println("Plum Hand: " + PlumP.getHandString());
 
+        wait(5);
 
     boolean done = false;
-    wait(3);
+    wait(2);
     clear();
    while(!done) {
     
     //Mustard
+    clear();
+    v.printBoard();
+    System.out.println("MUSTARDS TURN \n");
+    System.out.println("Mustard Hand: " + MustardP.getHandString());
+
     int rand = (int)(Math.random() * 6) % 6 + 1;
-    int r = rand;
+    int r = rand * 2;
     System.out.println();
     System.out.println();
-    System.out.println("You rolled a " + r + "! - " + "Choose direction to move");
-    System.out.println("1 - Up");
-    System.out.println("2 - Down");
-    System.out.println("3 - Right");
-    System.out.println("4 - Left");
-
+    System.out.println("Pick What To Do: \n");
+    System.out.println("1 - Move");
+    if(MustardP.getRoomIn() != "Outside") System.out.println("2 - Guess");
+    if(MustardP.getRoomIn().equals("Stairs")) System.out.println("3 - Accuse");
     Scanner scanner = new Scanner(System.in);
-    int dir = scanner.nextInt();
+    int choice = scanner.nextInt();
+    switch(choice) {
+        case 1:
+            clear();
+            v.printBoard();
+            System.out.println("You rolled a " + r/2 + "! - " + "Choose direction to move: \n");
+            System.out.println("1 - Up");
+            System.out.println("2 - Down");
+            System.out.println("3 - Right");
+            System.out.println("4 - Left");
+            System.out.println("5 - No Move");
+            c.move(MustardP, r);
+            clear();
+            v.printBoard();
+            break;
+        case 2:
+            System.out.println("Mustard Hand: " + MustardP.getHandString());
+            System.out.println("Pick the Number that Corrosponds to the Card in the Deck above (ex. First card = 1): \n");
+            System.out.println(d.getSetString() + "\n\n");
+            System.out.println("Pick a Character Card: \n");
+            Scanner scanner1 = new Scanner(System.in);
+            int charIndex = scanner1.nextInt();
+            System.out.println("Pick a Room Card: \n");
+            Scanner scanner2 = new Scanner(System.in);
+            int roomIndex = scanner2.nextInt();
+            System.out.println("Pick a Weapon Card: \n");
+            Scanner scanner3 = new Scanner(System.in);
+            int weaponIndex = scanner3.nextInt();
+            c.Guess(MustardP, ScarletP, PlumP, d.getHand().get(charIndex - 1), d.getHand().get(roomIndex - 1), d.getHand().get(weaponIndex - 1));
 
-    System.out.println(m.getTile(MustardP.getRow() + r, MustardP.getCol()));
-    c.move(MustardP, dir, r);
+            if(ScarletP.getHand().contains(deck.get(charIndex - 1))) System.out.println(ScarletP.getCharName() + " has " + deck.get(charIndex - 1).getName());
+            if(ScarletP.getHand().contains(deck.get(roomIndex - 1))) System.out.println(ScarletP.getCharName() + " has " + deck.get(roomIndex - 1).getName());
+            if(ScarletP.getHand().contains(deck.get(weaponIndex - 1))) System.out.println(ScarletP.getCharName() + " has " + deck.get(weaponIndex - 1).getName());
+            if(PlumP.getHand().contains(deck.get(charIndex - 1))) System.out.println(PlumP.getCharName() + " has " + deck.get(charIndex - 1).getName());
+            if(PlumP.getHand().contains(deck.get(roomIndex - 1))) System.out.println(PlumP.getCharName() + " has " + deck.get(roomIndex - 1).getName());
+            if(PlumP.getHand().contains(deck.get(weaponIndex - 1))) System.out.println(PlumP.getCharName() + " has " + deck.get(weaponIndex - 1).getName());
+
+            wait(5);
+            System.out.println(MustardP.getHandString());
+            wait(5);
+        break;
+        case 3:
+        System.out.println("Mustard Hand: " + MustardP.getHandString());
+        if(c.Accuse(PlumP, ENDGAME.get(0), ENDGAME.get(1), ENDGAME.get(2))) {
+            System.out.println("Prof. Plum Wins!");
+            done = true;
+        }
+        else System.out.println("WRONG GUESS - HUGE L");
+        wait(5);
+        break;
+        default:
+        System.out.println("Invalid Input - Turn Lost");
+    }
+
 
 
 
     //Scarlet
+    clear();
+    v.printBoard();
+    System.out.println("SCARLETS TURN \n");
+    System.out.println("Scarlet Hand: " + ScarletP.getHandString());
 
-    //Plum
+    int randS = (int)(Math.random() * 6) % 6 + 1;
+    int rS = randS * 2;
+    System.out.println();
+    System.out.println();
+    System.out.println("Pick What To Do: \n");
+    System.out.println("1 - Move");
+    if(ScarletP.getRoomIn() != "Outside") System.out.println("2 - Guess");
+    if(ScarletP.getRoomIn().equals("Stairs")) System.out.println("3 - Accuse");
+    Scanner scannerS = new Scanner(System.in);
+    int choiceS = scannerS.nextInt();
+    switch(choiceS) {
+        case 1:
+            clear();
+            v.printBoard();
+            System.out.println("Scarlet Hand: " + ScarletP.getHandString());
+            System.out.println("You rolled a " + rS/2 + "! - " + "Choose direction to move: \n");
+            System.out.println("1 - Up");
+            System.out.println("2 - Down");
+            System.out.println("3 - Right");
+            System.out.println("4 - Left");
+            System.out.println("5 - No Move");
+            c.move(ScarletP, rS);
+            clear();
+            v.printBoard();
+            break;
+        case 2:
+            System.out.println("Scarlet Hand: " + ScarletP.getHandString());
+            System.out.println("Pick the Number that Corrosponds to the Card in the Deck above (ex. First card = 1): \n");
+            System.out.println(d.getSetString() + "\n\n");
+            System.out.println("Pick a Character Card: \n");
+            Scanner scanner1 = new Scanner(System.in);
+            int charIndex = scanner1.nextInt();
+            System.out.println("Pick a Room Card: \n");
+            Scanner scanner2 = new Scanner(System.in);
+            int roomIndex = scanner2.nextInt();
+            System.out.println("Pick a Weapon Card: \n");
+            Scanner scanner3 = new Scanner(System.in);
+            int weaponIndex = scanner3.nextInt();
+            c.Guess(MustardP, ScarletP, PlumP, d.getHand().get(charIndex - 1), d.getHand().get(roomIndex - 1), d.getHand().get(weaponIndex - 1));
+
+            if(MustardP.getHand().contains(deck.get(charIndex - 1))) System.out.println(MustardP.getCharName() + " has " + deck.get(charIndex - 1).getName());
+            if(MustardP.getHand().contains(deck.get(roomIndex - 1))) System.out.println(MustardP.getCharName() + " has " + deck.get(roomIndex - 1).getName());
+            if(MustardP.getHand().contains(deck.get(weaponIndex - 1))) System.out.println(MustardP.getCharName() + " has " + deck.get(weaponIndex - 1).getName());
+            if(PlumP.getHand().contains(deck.get(charIndex - 1))) System.out.println(PlumP.getCharName() + " has " + deck.get(charIndex - 1).getName());
+            if(PlumP.getHand().contains(deck.get(roomIndex - 1))) System.out.println(PlumP.getCharName() + " has " + deck.get(roomIndex - 1).getName());
+            if(PlumP.getHand().contains(deck.get(weaponIndex - 1))) System.out.println(PlumP.getCharName() + " has " + deck.get(weaponIndex - 1).getName());
+
+            wait(5);
+        break;
+        case 3:
+        System.out.println("Scarlet Hand: " + ScarletP.getHandString());
+        if(c.Accuse(PlumP, ENDGAME.get(0), ENDGAME.get(1), ENDGAME.get(2))) {
+            System.out.println("Prof. Plum Wins!");
+            done = true;
+        }
+        else System.out.println("WRONG GUESS - HUGE L");
+        wait(5);
+        break;
+        default:
+        System.out.println("Invalid Input - Turn Lost");
+    }
+    // Plum
+    clear();
+    v.printBoard();
+    System.out.println("PLUMS TURN \n");
+    System.out.println("Plum Hand: " + PlumP.getHandString());
+
+    int randP = (int)(Math.random() * 6) % 6 + 1;
+    int rP = randP * 2;
+    System.out.println();
+    System.out.println();
+    System.out.println("Pick What To Do: \n");
+    System.out.println("1 - Move");
+    if(PlumP.getRoomIn() != "Outside") System.out.println("2 - Guess");
+    if(PlumP.getRoomIn().equals("Stairs")) System.out.println("3 - Accuse");
+    Scanner scannerP = new Scanner(System.in);
+    int choiceP = scannerP.nextInt();
+    switch(choiceP) {
+        case 1:
+            clear();
+            v.printBoard();
+            System.out.println("Plum Hand: " + PlumP.getHandString());
+            System.out.println("You rolled a " + rP/2 + "! - " + "Choose direction to move: \n");
+            System.out.println("1 - Up");
+            System.out.println("2 - Down");
+            System.out.println("3 - Right");
+            System.out.println("4 - Left");
+            System.out.println("5 - No Move");
+            c.move(PlumP, rP);
+            clear();
+            v.printBoard();
+            break;
+        case 2:
+            System.out.println("Plum Hand: " + PlumP.getHandString());
+            System.out.println("Pick the Number that Corrosponds to the Card in the Deck above (ex. First card = 1): \n");
+            System.out.println(d.getSetString() + "\n\n");
+            System.out.println("Pick a Character Card: \n");
+            Scanner scanner1 = new Scanner(System.in);
+            int charIndex = scanner1.nextInt();
+            System.out.println("Pick a Room Card: \n");
+            Scanner scanner2 = new Scanner(System.in);
+            int roomIndex = scanner2.nextInt();
+            System.out.println("Pick a Weapon Card: \n");
+            Scanner scanner3 = new Scanner(System.in);
+            int weaponIndex = scanner3.nextInt();
+            c.Guess(MustardP, ScarletP, PlumP, d.getHand().get(charIndex - 1), d.getHand().get(roomIndex - 1), d.getHand().get(weaponIndex - 1));
+
+            if(MustardP.getHand().contains(deck.get(charIndex - 1))) System.out.println(MustardP.getCharName() + " has " + deck.get(charIndex - 1).getName());
+            if(MustardP.getHand().contains(deck.get(roomIndex - 1))) System.out.println(MustardP.getCharName() + " has " + deck.get(roomIndex - 1).getName());
+            if(MustardP.getHand().contains(deck.get(weaponIndex - 1))) System.out.println(MustardP.getCharName() + " has " + deck.get(weaponIndex - 1).getName());
+            if(ScarletP.getHand().contains(deck.get(charIndex - 1))) System.out.println(ScarletP.getCharName() + " has " + deck.get(charIndex - 1).getName());
+            if(ScarletP.getHand().contains(deck.get(roomIndex - 1))) System.out.println(ScarletP.getCharName() + " has " + deck.get(roomIndex - 1).getName());
+            if(ScarletP.getHand().contains(deck.get(weaponIndex - 1))) System.out.println(ScarletP.getCharName() + " has " + deck.get(weaponIndex - 1).getName());
+
+            wait(5);
+        break;
+        case 3:
+        System.out.println("Plum Hand: " + PlumP.getHandString());
+        if(c.Accuse(PlumP, ENDGAME.get(0), ENDGAME.get(1), ENDGAME.get(2))) {
+            System.out.println("Prof. Plum Wins!");
+            done = true;
+        }
+        else System.out.println("WRONG GUESS - HUGE L");
+        wait(5);
+        break;
+        default:
+        System.out.println("Invalid Input - Turn Lost");
+    }
 
    }
 
@@ -141,6 +343,7 @@ public class Main {
         }
    }
 
+   
    public static void clear() {
     System.out.print("\033[H\033[2J");
     System.out.flush();
