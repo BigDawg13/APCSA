bec111d59bbd6cc571d721113d665597d36d944f
BigDawg13 <148258713+BigDawg13@users.noreply.github.com>
2023-12-16 23:11:05 -0600

    Update Card.java - Final Commit From Home

--- 2D Game/Card.java
+++ 2D Game/Card.java
@@ -1,12 +1,12 @@
 public class Card {
-   public String type;
-   public String name;
-  
-   Card(String type, String name) {
-       this.type = type;
-       this.name = name;
-   }
-   Card(){}
-   public String getName() { return name; }
-   public String getType() { return type; }
-}
+    public String type;
+    public String name;
+   
+    Card(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+    Card(){}
+    public String getName() { return name; }
+    public String getType() { return type; }
+ }
